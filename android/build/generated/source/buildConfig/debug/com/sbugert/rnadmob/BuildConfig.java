2051f125a3aeb3c2197eb01d80242c82170961dd
loicx <xuanloi1408@gmail.com>
2020-08-12 16:11:38 +0700

    first commit

---
+++ android/build/generated/source/buildConfig/debug/com/sbugert/rnadmob/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sbugert.rnadmob;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.sbugert.rnadmob";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.sbugert.rnadmob";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
